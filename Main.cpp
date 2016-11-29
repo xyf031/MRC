6078f693c545d7331960faccd5f5c5e0b39e9130
X031 <XYF@XiaoYifan.local>
2016-11-29 21:23:27 +0800

    mode3() In main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,12 +8,20 @@
 #include "readMrcStar.h"
 void mode1 (const char* fileRoot);
 void mode2 (const char* fileRoot);
+void mode3 (const char* fileRoot);
 
 
 int main(int argc, char* argv[]) {
 	switch (argc) {
 		case 1: mode1("."); break;
 		case 2: mode1(argv[1]); break;
+		case 3: 
+			if (*argv[2] == '0') {
+				mode3(argv[1]);
+			} else {
+				mode2(argv[1]);
+			}
+			break;
 		default: mode2(argv[1]); break;
 	}
 	return 0;
@@ -54,4 +62,20 @@ void mode2 (const char* fileRoot) {
 	}
 	fclose(fmrc);
 	fclose(fstar);
+}
+
+void mode3 (const char* fileRoot) {
+	char shell[FILE_NAME_LENGTH];
+	sprintf(shell, "ls %s/*.mrc > %s/all_mrc.tmp", fileRoot, fileRoot);
+	system(shell);
+
+	FILE *f = fopen("all_mrc.tmp", "r");
+	char mrcPath[FILE_NAME_LENGTH];
+	while (fscanf(f, "%s", mrcPath) > 0) {
+		read_MRC_And_Star(mrcPath, false, true);
+		// printf("MRC modified:\t\t\t%s\n", mrcPath);
+	}
+	fclose(f);
+	sprintf(shell, "rm %s/all_mrc.tmp", fileRoot);
+	system(shell);
 }
